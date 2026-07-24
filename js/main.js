cc1f133164c6629f849b32201a047a010142e915
FAMA <sarrfa@gmail.com>
2026-07-24 10:01:17 +0000

    Ajout complet du dark mode (toggle JS, persistance localStorage, ajustements CSS)

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
 const yearElement = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
+// /DARK MODE / LIGHT MODE
+const themeToggleButton = document.querySelector(".theme-toggle");
+const htmlElement = document.documentElement;
+
+themeToggleButton.addEventListener("click", function () {
+    if (htmlElement.getAttribute("data-theme") === "dark") {
+        htmlElement.removeAttribute("data-theme");
+        localStorage.setItem("theme", "light");
+    } else {
+        htmlElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+});
+
+// Au chargement de la page, on vérifie si un thème était déjà choisi avant
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+    htmlElement.setAttribute("data-theme", "dark");
+}
